974001c0068b33d345b32374932180579e9bf067
Austin Michaud <austin@sigparser.com>
2021-10-06 13:33:24 -0700

    place down tiles

---
+++ assets/_beach-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="_beach-tiles" tilewidth="8" tileheight="8" tilecount="12" columns="4">
+ <image source="_beach-tiles.png" width="32" height="24"/>
+</tileset>
